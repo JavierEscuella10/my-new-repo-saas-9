461edff428c12898aa5810638d059fa2d387eacf
JavierEscuella10 <bala.subramanian@avasoft.com>
2024-09-20 01:20:33 +0530

    Add/Update Email/Model/output_components.py

---
+++ Email/Model/output_components.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+txt_response = {"contentType": "txt", "content": ""}
+
+chat_conversation_response = {
+    "order": 1,
+    "role": "bot",
+    "meta": {
+        "name": "Service Help Desk AI BOT",
+        "timeStamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f"),
+    },
+    "data": {},
+    
+}
+
+
+
+language="English"
