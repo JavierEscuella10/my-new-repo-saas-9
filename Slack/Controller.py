f3e53a6b0c04198ae70d1ff364fd2a4b75b307ec
JavierEscuella10 <bala.subramanian@avasoft.com>
2024-09-20 01:21:28 +0530

    Add/Update Slack/Controller.py

---
+++ Slack/Controller.py
@@ -0,0 +1,37 @@
+import jwt
+import json,request
+from decouple import config
+from psycopg2 import sql
+from datetime import datetime, timedelta
+from Validators.processConversation import processConversation
+from flask import Flask , request
+from flask_cors import CORS
+from Datasource.Jira.Story.kafka_consumer import jira_story_consumer_message 
+  #import
+
+app =Flask(__name__)
+CORS(app)
+@app.route('/process_conversation', methods=['POST'])
+def process_conversation():
+    reqData = request.get_json()
+    return processConversation(reqData)
+
+
+
+
+
+
+
+    @app.route('/jira_story_consumer_message', methods=['POST'])
+    def process_conversation():
+        reqData = request.get_json()
+        return jira_story_consumer_message()
+
+    #add_function
+
+
+
+    
+
+
+
